218a6847ec9bed74b4cf039e9cd70dd3f4511675
Victor Vermot <victorvermot@gmail.com>
2025-03-27 11:22:16 +0100

    plateformes and bam

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -6,13 +6,18 @@ var level_origin: Vector2
 var background_color := Color(0.1333, 0.1137, 0.3255, 1.0)
 var spawn_position: Vector2
 
+# PLATEFORM LOGIC
+
+var is_player_safe: bool
+var plateform_count: int
+
 func _ready() -> void:
 	$Area2D.add_to_group("player")
 	spawn_position = global_position
 	_get_level_image_info()
 	Events.on_player_died.connect(on_player_death)
 
-func _get_level_image_info():
+func _get_level_image_info() -> void:
 	var tex = preload("res://Sprites/LEVEL.png") as CompressedTexture2D
 	level_image = tex.get_image()
 	level_origin = %Level.global_position - tex.get_size() / 2
@@ -23,7 +28,7 @@ func _process(delta: float) -> void:
 
 	var pixel_color = level_image.get_pixelv(image_pos)
 	if is_inside_image(image_pos):
-		if is_on_death_color(pixel_color):
+		if is_on_death_color(pixel_color) and not is_player_safe:
 			Events.on_player_died.emit()
 		if should_follow_mouse:
 			global_position = get_global_mouse_position()
@@ -35,17 +40,29 @@ func _on_area_2d_input_event(viewport: Node, event: InputEvent, shape_idx: int) 
 	if event.is_action_pressed("mouse_pressed"):
 		should_follow_mouse = true
 
-func maze_image_size_to_pixel(world_pos: Vector2) -> Vector2i:
-	return Vector2i(world_pos.round())
-
 func is_on_death_color(color: Color, tolerance := 0.05) -> bool:
 	return abs(color.r - background_color.r) <= tolerance and \
 		   abs(color.g - background_color.g) <= tolerance and \
 		   abs(color.b - background_color.b) <= tolerance
 
-func on_player_death():
+func on_player_death() -> void:
 	should_follow_mouse = false
 	global_position = spawn_position
 
 func set_respawn_point(new_pos: Vector2) -> void:
 	spawn_position = new_pos
+
+
+# PLATEFORME LOGIC
+
+func _on_area_2d_area_entered(area: Area2D) -> void:
+	if area.is_in_group("plateforme"):
+		is_player_safe = true
+		plateform_count += 1
+
+
+func _on_area_2d_area_exited(area: Area2D) -> void:
+	if area.is_in_group("plateforme"):
+		if plateform_count == 0:
+			is_player_safe = false
+		plateform_count -= 1
