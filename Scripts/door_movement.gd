cf34658da41327a7895c56a09cffc13aa6a60abd
Victor Vermot <victorvermot@gmail.com>
2025-03-29 18:23:20 +0100

    fix door hitbox

--- Scripts/door_movement.gd
+++ Scripts/door_movement.gd
@@ -6,6 +6,8 @@ var spawn_position: Vector2
 @onready var timer: Timer = $Timer
 @onready var door_slam_sound: AudioStreamPlayer2D = $DoorSlamSound
 @export var initial_delay: float
+@export var move_towards_left := false
+var speed := 10
 
 func _ready() -> void:
 	spawn_position = position
@@ -13,11 +15,13 @@ func _ready() -> void:
 	await get_tree().create_timer(initial_delay).timeout
 	timer.start()
 	should_move = true
+	if move_towards_left:
+		speed *= -1
 
 
 func _process(delta: float) -> void:
 	if should_move:
-		position.x += delta * 10
+		position.x += delta * speed
 
 func reset_door_position():
 	timer.stop()
